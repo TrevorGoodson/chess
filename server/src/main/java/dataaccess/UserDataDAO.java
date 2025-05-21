1320abcb0678e51f4f0e6a6b48c3bc9a51cffec5
TrevorGoodson <trevordgoodson@gmail.com>
2025-05-20 23:38:36 -0600

    added getUser, createUser, and clear methods to UserDataDAO

--- server/src/main/java/dataaccess/UserDataDAO.java
+++ server/src/main/java/dataaccess/UserDataDAO.java
@@ -8,5 +8,32 @@ public class UserDataDAO {
 
     public UserDataDAO() {}
 
+    /**
+     * Finds a user in the database.
+     * @param username the username of the user.
+     * @return the user data or null if none is found.
+     */
+    public UserData getUser(String username) {
+        for (var userDatum : userData) {
+            if (userDatum.username().equals(username)) {
+                return userDatum;
+            }
+        }
+        return null;
+    }
 
+    /**
+     * Adds a new user to the database.
+     * @param newUser the user data to be added
+     */
+    public void createUser(UserData newUser) {
+        userData.add(newUser);
+    }
+
+    /**
+     * Clears all users from the database.
+     */
+    public void clear() {
+        userData.clear();
+    }
 }
