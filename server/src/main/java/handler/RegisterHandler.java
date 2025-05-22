efe392e06619d6d87c2b78446b7cc0f859f333dc
TrevorGoodson <trevordgoodson@gmail.com>
2025-05-22 15:21:18 -0600

    implemented the RegisterHandler

--- server/src/main/java/handler/RegisterHandler.java
+++ server/src/main/java/handler/RegisterHandler.java
@@ -1,10 +1,15 @@
 package handler;
 
+import com.google.gson.Gson;
+import requestresult.RegisterRequest;
+import service.UserService;
 import spark.*;
 
 public class RegisterHandler implements Route {
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        return null;
+        var registerRequest = new Gson().fromJson(request.body(), requestresult.RegisterRequest.class);
+        var registerResult = new UserService().register(registerRequest);
+        return new Gson().toJson(registerResult);
     }
 }
