467792f22def6eae3201ef8911481dfee1b3abe1
TrevorGoodson <trevordgoodson@gmail.com>
2025-05-21 23:21:43 -0600

    made all the handler classes

---
+++ server/src/main/java/handler/ClearHandler.java
@@ -0,0 +1,14 @@
+package handler;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import service.ClearService;
+
+public class ClearHandler implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        new ClearService().clear();
+        return null;
+    }
+}
---
+++ server/src/main/java/handler/RegisterHandler.java
@@ -0,0 +1,10 @@
+package handler;
+
+import spark.*;
+
+public class RegisterHandler implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
