07c57e28a4dc3eef2c2861932b290544998bd55b
TrevorGoodson <trevordgoodson@gmail.com>
2025-05-28 22:11:52 -0600

    selectTest in AuthDataDAOSQLTests

--- server/src/test/java/dataaccess/AuthDataDAOSQLTests.java
+++ server/src/test/java/dataaccess/AuthDataDAOSQLTests.java
@@ -4,6 +4,8 @@ import dataaccess.*;
 import model.AuthData;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AuthDataDAOSQLTests {
     @Test
     public void insertTest() {
@@ -14,4 +16,20 @@ public class AuthDataDAOSQLTests {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    public void selectTest() {
+        AuthDataDAO authDataDAO = new AuthDataDAOSQL();
+        try {
+            String authToken = "sample_auth_token";
+            String username = "sample_username";
+            //authDataDAO.addAuthData(new AuthData(authToken, username));
+            AuthData a = authDataDAO.getAuthData(authToken);
+            assertEquals(authToken, a.authToken());
+            assertEquals(username, a.username());
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
