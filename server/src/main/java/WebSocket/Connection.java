54bc6acae60286e650edbda8f94118f19e6c3082
TrevorGoodson <trevordgoodson@gmail.com>
2025-06-11 13:49:09 -0600

    Created the Connection class on the server side

---
+++ server/src/main/java/WebSocket/Connection.java
@@ -0,0 +1,21 @@
+package WebSocket;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+
+public class Connection {
+    public String username;
+    public Session session;
+
+    public Connection(String username, Session session) {
+        this.username = username;
+        this.session = session;
+    }
+
+    public void send(ServerMessage message) throws IOException {
+        session.getRemote().sendString(new Gson().toJson(message));
+    }
+}
